d2d53aa258876065b5de5cd13d33a9254e76f013
Matt Behrens <askedrelic@gmail.com>
2016-01-02 19:32:23 +0100

    add some more interesting packages

--- recipes/extra-packages.rb
+++ recipes/extra-packages.rb
@@ -1,9 +1,15 @@
 # Default packages
 
+# Essentials
 package "git"
 package "vim"
-package "tree"
 package "curl"
+package "whois"
+package "tree"
+package "tcpdump"
+
+# Better than top
+package 'htop'
 
 # Just in case...
 package "openssh-server"
@@ -15,18 +21,44 @@ package "tmux"
 # Enable ssh blocking by default
 package 'fail2ban'
 
-package "tcpdump"
-# for reverse ssh
+# for reverse ssh tunnels
 package "autossh"
 
-# ftp clients
+# Ftp clients
 package "yafc"
 package "lftp"
 
-package 'sysstat'  # Monitor io
-package 'nethogs'  # Monitor network
-package 'htop'     # Better than top
-package "whois"
+# Useful monitoring utils
+# sar: collects and reports system activity information;
+# iostat: reports CPU utilization and disk I/O statistics;
+# mpstat: reports global and per-processor statistics;
+# pidstat: reports statistics for Linux tasks (processes);
+# sadf: displays data collected by sar in various formats;
+# nfsiostat: reports I/O statistics for network filesystems;
+# cifsiostat: reports I/O statistics for CIFS filesystems.
+package 'sysstat'
 
-# disabled heavy audio/video dependencies
+# Top for network traffic
+package 'nethogs'
+
+# Useful utils
+# chronic: runs a command quietly unless it fails
+# combine: combine the lines in two files using boolean operations
+# errno: look up errno names and descriptions
+# ifdata: get network interface info without parsing ifconfig output
+# ifne: run a program if the standard input is not empty
+# isutf8: check if a file or standard input is utf-8
+# lckdo: execute a program with a lock held
+# mispipe: pipe two commands, returning the exit status of the first
+# parallel: run multiple jobs at once
+# pee: tee standard input to pipes
+# sponge: soak up standard input and write to a file
+# ts: timestamp standard input
+# vidir: edit a directory in your text editor
+# vipe: insert a text editor into a pipe
+# zrun: automatically uncompress arguments to command
+package 'moreutils'
+
+
+# disabled from heavy audio/video dependencies
 # package "youtube-dl"
